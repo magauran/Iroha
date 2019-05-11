b0ba4f9349717cce121065886dbbcd03376add1e
Alexey Salangin <a7ls@ya.ru>
2019-05-11 04:48:24 +0300

    Create Xcode project

---
+++ Iroha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Iroha
+//
+//  Created by Alexey Salangin on 11/05/2019.
+//  Copyright © 2019 Alexey Salangin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
